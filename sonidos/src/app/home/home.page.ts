438a9a9212edf4e72a976765bfd74852b792d9d9
Diego <jd.sarmientoh.7@gmail.com>
2019-08-29 18:08:08 -0500

    123

--- sonidos/src/app/home/home.page.ts
+++ sonidos/src/app/home/home.page.ts
@@ -10,21 +10,54 @@ import { Animal } from "../../interfaces/animal.interface";
 export class HomePage {
 
   public animales:Animal[] = [];
+  public audio = new Audio;
+  audioTiempo: any;
 
   constructor() {
 
-    this.animales=ANIMALES.splice(0);
+    this.animales=ANIMALES.slice(0);
 
   }
 
   reproducir( animal:Animal){
-    console.log(animal);
-    let audio = new Audio();
-    audio.src = animal.audio;
-    audio.load();
-    audio.play();
+    this.pausar_audio(animal);
+    if (animal.reproduciendo) {
+        animal.reproduciendo = false;
+        return;
+    }
+    this.audio.src = animal.audio;
+    this.audio.load();
+    this.audio.play();
     animal.reproduciendo = true;
     setTimeout(()=> animal.reproduciendo = false, animal.duracion * 1000);
+
   }
 
+   pausar_audio(animalSel:Animal){
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime=0;
+
+    for (let animal of this.animales) {
+      if (animal.nombre != animalSel.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
+    animalSel.reproduciendo=false;
+  }
+
+  borrar_animal(id:number){
+    this.animales.splice(id,1);
+    console.log("animales",this.animales);
+    console.log("index",id);
+  }
+
+  recargar_animales(refresher:any){
+    console.log("inicio del refresh");
+    setTimeout(() => {
+        console.log("termino el refresh");
+        this.animales=ANIMALES.slice(0);
+        refresher.target.complete();
+    }, 1500);
+  }
 }
